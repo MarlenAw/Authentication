c69a81de2e9ccdd9dcf0792773c5eaa64d6a3018
MarlenAwwad <marlenawsh3@gmail.com>
2017-12-26 13:53:37 +0200

    database is linked

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET home page
 router.get('/', (req, res, next) => {
-  res.render('home', {title: 'This is HOME'});
+  res.render('home', {title: 'Login'});
 });
 
 module.exports = router;
